c8d3b984234f9d0b5a469afb1f09d17086114334
brunoabneves <brunoabneves97@gmail.com>
2021-07-28 22:52:38 -0300

    Usando Spring Data JPA

--- src/main/java/com/bruno/api/controller/UsuarioController.java
+++ src/main/java/com/bruno/api/controller/UsuarioController.java
@@ -1,32 +1,26 @@
 package com.bruno.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bruno.domain.model.Usuario;
+import com.bruno.domain.repository.UsuarioRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class UsuarioController {
 	
+	
+	private UsuarioRepository usuarioRepository;
+	
 	@GetMapping("/users")
 	public List<Usuario> listar(){
-		var usuario1 = new Usuario();
-		usuario1.setId(1L);
-		usuario1.setNome("Elwe Singolo");
-		usuario1.setEmail("capacinzenta@email.com");
-		usuario1.setSenha("thingol");
 		
-		
-		var usuario2 = new Usuario();
-		usuario2.setId(2L);
-		usuario2.setNome("Feanor");
-		usuario2.setEmail("feanor@email.com");
-		usuario2.setSenha("silmarill");
-		
-		return Arrays.asList(usuario1, usuario2);
+		return usuarioRepository.findAll();
 	}
 	
 }

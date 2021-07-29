d5319121df5d7f854a9469b9e72b956853b98bf1
brunoabneves <brunoabneves97@gmail.com>
2021-07-28 21:10:24 -0300

    Implementando Collection Resource de teste

---
+++ src/main/java/com/bruno/domain/model/Usuario.java
@@ -0,0 +1,15 @@
+package com.bruno.domain.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Usuario {
+
+	private Long id;
+	private String nome;
+	private String email;
+	private String senha;
+	
+}

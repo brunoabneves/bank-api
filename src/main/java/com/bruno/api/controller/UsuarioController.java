99e48200b2843af49c7816727157981c838042f9
brunoabneves <brunoabneves97@gmail.com>
2021-07-28 23:55:09 -0300

    Implementando validação com Bean Validation

--- src/main/java/com/bruno/api/controller/UsuarioController.java
+++ src/main/java/com/bruno/api/controller/UsuarioController.java
@@ -2,7 +2,14 @@ package com.bruno.api.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bruno.domain.model.Usuario;
@@ -12,15 +19,22 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/users")
 public class UsuarioController {
 	
 	
 	private UsuarioRepository usuarioRepository;
 	
-	@GetMapping("/users")
+	@GetMapping
 	public List<Usuario> listar(){
 		
 		return usuarioRepository.findAll();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Usuario cadastrar(@Valid @RequestBody Usuario usuario) {
+		return usuarioRepository.save(usuario);
 	}
 	
 }

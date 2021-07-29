051f52c2802a8a4befe1aac163d67192f72867c8
brunoabneves <brunoabneves97@gmail.com>
2021-07-29 15:38:25 -0300

    Autenticação com JWTToken

--- src/main/java/com/bruno/config/SecurityConfig.java
+++ src/main/java/com/bruno/config/SecurityConfig.java
@@ -1,15 +1,17 @@
 package com.bruno.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.bruno.domain.service.CustomUserDetailsService;
+
+import static com.bruno.config.SecurityConstantes.*;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -20,12 +22,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
-		http.authorizeRequests()
-				.anyRequest().authenticated()
+		http.cors().and().csrf().disable().authorizeRequests()
+				.antMatchers(HttpMethod.GET, SIGN_UP_URL).permitAll()
 				.and()
-				.httpBasic()
-				.and()
-				.csrf().disable();
+				.addFilter(new JWTAuthenticationFilter(authenticationManager()))
+				.addFilter(new JWTAuthorizationFilter(authenticationManager(), customUserDetailsService));
 	}
 	
 	@Override

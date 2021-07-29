88615d934a3efc03da5be4d1e87049df2257ac97
brunoabneves <brunoabneves97@gmail.com>
2021-07-28 22:26:42 -0300

    Configurando o Flyway

---
+++ src/main/resources/db/migration/V001__cria-tabela-usuario.sql
@@ -0,0 +1,8 @@
+CREATE TABLE usuario (
+	id BIGINT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(60) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+	senha VARCHAR(255) NOT NULL,
+    
+    PRIMARY KEY (id)
+);
